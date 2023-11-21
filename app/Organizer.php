07db594db6508311775f7282bbea4a584bce5c97
Thahirudin <tohiruzain098@gmail.com>
2023-11-21 15:35:24 +0700

    tambah tbl_organizer dan model

---
+++ app/Organizer.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Organizer extends Model
+{
+    protected $fillable = [
+        'nama',
+        'tanggal_lahir',
+        'email',
+        'password',
+    ];
+
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    public function events()
+    {
+        return $this->hasMany('App\Event');
+    }
+}
