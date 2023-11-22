971342692736b4a4379d893150859842586a5b5a
cipaanumee <fadhlinahanum375@gmail.com>
2023-11-22 21:21:59 +0700

    perbarui side bar

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -19,46 +19,28 @@
                <ul id="iq-sidebar-toggle" class="iq-menu">
                   <li><a href="index.html" class="text-primary"><i class="ri-arrow-right-line"></i><span>Visit site</span></a></li>
                   <li class="active active-menu"><a href="index.html" class="iq-waves-effect"><i class="las la-home iq-arrow-left"></i><span>Dashboard</span></a></li>
-                  <li><a href="user.html" class="iq-waves-effect"><i class="las la-user-friends"></i><span>User</span></a></li>
-                  <li><a href="pages-pricing.html" class="iq-waves-effect"><i class="ri-price-tag-line"></i><span>Pricing</span></a></li>
+                  <li><a href="order.html" class="iq-waves-effect"><i class="ri-price-tag-line"></i><span>Order</span></a></li>
+                  <li><a href="kategori.html" class="iq-waves-effect"><i class="ri-price-tag-line"></i><span>Kategori</span></a></li>
                   <li>
                      <a href="#event" class="iq-waves-effect collapsed" data-toggle="collapse" aria-expanded="false"><i class="lab la-elementor iq-arrow-left"></i><span>Event</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
                      <ul id="event" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
-                        <li class="elements">
-                           <a href="#sub-menu" class="iq-waves-effect"><i class="ri-play-circle-line"></i><span>List Event</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                        </li>
+                        <li><a href="list.html"><i class="las la-sign-in-alt"></i>List Event</a></li>
+                        <li><a href="event_akan_datang.html"><i class="ri-login-circle-line"></i>Event Akan Datang</a></li>
+                        <li><a href="event_selesai.html"><i class="ri-login-circle-line"></i>Event Selesai</a></li>
+                        
                      </ul>
                   </li>
                   <li>
-                     <a href="#pages" class="iq-waves-effect collapsed" data-toggle="collapse" aria-expanded="false"><i class="las la-file-alt iq-arrow-left"></i><span>Pages</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                     <ul id="pages" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
-                        <li>
-                           <a href="#authentication" class="iq-waves-effect collapsed" data-toggle="collapse" aria-expanded="false"><i class="ri-pages-line"></i><span>Authentication</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                           <ul id="authentication" class="iq-submenu collapse" data-parent="#pages">
-                              <li><a href="sign-in.html"><i class="las la-sign-in-alt"></i>Login</a></li>
-                              <li><a href="sign-up.html"><i class="ri-login-circle-line"></i>Register</a></li>
-                              <li><a href="pages-recoverpw.html"><i class="ri-record-mail-line"></i>Recover Password</a></li>
-                              <li><a href="pages-confirm-mail.html"><i class="ri-file-code-line"></i>Confirm Mail</a></li>
-                              <li><a href="pages-lock-screen.html"><i class="ri-lock-line"></i>Lock Screen</a></li>
-                           </ul>
-                        </li>
-                        <li>
-                           <a href="#extra-pages" class="iq-waves-effect collapsed" data-toggle="collapse" aria-expanded="false"><i class="ri-pantone-line"></i><span>Extra Pages</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
-                           <ul id="extra-pages" class="iq-submenu collapse" data-parent="#pages">
-                              <li><a href="pages-timeline.html"><i class="ri-map-pin-time-line"></i>Timeline</a></li>
-                              <li><a href="pages-invoice.html"><i class="ri-question-answer-line"></i>Invoice</a></li>
-                              <li><a href="blank-page.html"><i class="ri-invision-line"></i>Blank Page</a></li>
-                              <li><a href="pages-error.html"><i class="ri-error-warning-line"></i>Error 404</a></li>
-                              <li><a href="pages-error-500.html"><i class="ri-error-warning-line"></i>Error 500</a></li>
-                           
-                              <li><a href="pages-maintenance.html"><i class="ri-archive-line"></i>Maintenance</a></li>
-                              <li><a href="pages-comingsoon.html"><i class="ri-mastercard-line"></i>Coming Soon</a></li>
-                              <li><a href="pages-faq.html"><i class="ri-compasses-line"></i>Faq</a></li>
-                           </ul>
-                        </li>
-                     </ul>
+                     <a href="#pengguna" class="iq-waves-effect collapsed" data-toggle="collapse" aria-expanded="false"><i class="las la-user-friends"></i><span>Pengguna</span><i class="ri-arrow-right-s-line iq-arrow-right"></i></a>
+                     <ul id="pengguna" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
+                        <li><a href="organizer.html"><i class="las la-sign-in-alt"></i>Organizer</a></li>
+                        <li><a href="member.html"><i class="ri-login-circle-line"></i>Member</a></li>
+                     </ul>          
                   </li>
+                  <li><a href="keuangan.html" class="iq-waves-effect"><i class="las la-file-alt iq-arrow-left"></i><span>Keuangan</span></a></li>
                </ul>
-            </nav>
-         </div>
-      </div>
+            </li>
+         </ul>
+      </nav>
+   </div>
+</div>
