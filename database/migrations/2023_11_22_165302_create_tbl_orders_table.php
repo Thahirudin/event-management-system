b82091aa77e50f498576be04460e1236a1139d85
iqbalprilyan <iqbalpriliyan@gmail.com>
2023-11-23 00:21:45 +0700

    tbl_orders

---
+++ database/migrations/2023_11_22_165302_create_tbl_orders_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTblOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tbl_orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('id_member')->Unique();
+            $table->integer('id_event')->Unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tbl_orders');
+    }
+}
