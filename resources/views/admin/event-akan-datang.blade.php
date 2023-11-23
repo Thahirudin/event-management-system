0ada310a8002a08948115fe4c8917fb99b8f35dd
Pathia30 <fathia.fathia467@gmail.com>
2023-11-23 20:40:31 +0700

    list event

--- resources/views/admin/event-akan-datang.blade.php
+++ resources/views/admin/event-akan-datang.blade.php
@@ -12,7 +12,72 @@
     active active-menu
 @endsection
 @section('content')
-
+<div class="row">
+                  <div class="col-sm-12">
+                     <div class="iq-card">
+                        <div class="iq-card-header d-flex justify-content-between">
+                           <div class="iq-header-title">
+                              <h4 class="card-title">List Event</h4>
+                           </div>
+                        </div>
+                        <div class="iq-card-body">
+                           <div class="table-responsive">
+                              <table id="datatable" class="table table-striped table-bordered" >
+                                 <thead>
+                                    <tr>
+                                       <th>Nama Event</th>
+                                       <th>Tanggal</th>
+                                       <th>Detail</th>
+                                       <th>Harga</th>
+                                       <th>Kontak</th>
+                                    </tr>
+                                 </thead>
+                                 <tbody>
+                                    <tr>
+                                       <td>JOYLAND JAKARTA 2023</td>
+                                       <td>24 Nov - 26 Nov 2023</td>
+                                       <td> Music and arts festival</td>
+                                       <td>Rp 488.000</td>
+                                       <td>0329373746</td>
+                                    </tr>
+                                    <tr>
+                                       <td>JOYLAND JAKARTA 2023</td>
+                                       <td>24 Nov - 26 Nov 2023</td>
+                                       <td>Music and arts festival</td>
+                                       <td>6Rp 488.000</td>
+                                       <td>0329373746</td>
+                                    </tr>
+                                    <tr>
+                                       <td>JOYLAND JAKARTA 2023</td>
+                                       <td>24 Nov - 26 Nov 2023</td>
+                                       <td>Music and arts festival</td>
+                                       <td>Rp 488.000</td>
+                                       <td>0329373746</td>
+                                    </tr>
+                                    <tr>
+                                       <td>JOYLAND JAKARTA 2023</td>
+                                       <td>24 Nov - 26 Nov 2023</td>
+                                       <td>Music and arts festival</td>
+                                       <td>Rp 488.000</td>
+                                       <td>0329373746</td>
+                                    </tr>
+                                    <tr>
+                                       <td>JOYLAND JAKARTA 2023</td>
+                                       <td>24 Nov - 26 Nov 2023</td>
+                                       <td>Music and arts festival</td>
+                                       <td>Rp 488.000</td>
+                                       <td>0329373746</td>
+                                    </tr>
+ 
+                              </table>
+                           </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
 @endsection
 @section('addJs')
     {{-- Masukkan dibawah ini jika ingin menambahkan JS --}}
