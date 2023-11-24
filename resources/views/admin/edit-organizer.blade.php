8eef9013d0f8cc9d0ce85219082137bed72dcd5d
cipaanumee <fadhlinahanum375@gmail.com>
2023-11-25 00:24:32 +0700

    Form tambah dan edit Organizer

--- resources/views/admin/edit-organizer.blade.php
+++ resources/views/admin/edit-organizer.blade.php
@@ -9,6 +9,34 @@
     active active-menu
 @endsection
 @section('content')
+<div class="iq-card">
+    <div class="iq-card-header d-flex justify-content-between text-white">
+        <div class="iq-header-title">
+            <h4 class="card-title">Edit Organizer</h4>
+        </div>
+    </div>
+    <div class="iq-card-body">
+        <form>
+            <div class="form-group">
+                <label for="nama">Nama</label>
+                <input type="text" class="form-control" id="nama">
+            </div>
+            <div class="form-group">
+                <label for="tgllahir">Tanggal lahir</label>
+                <input type="date" class="form-control" id="tgllahir">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" class="form-control" id="email">
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" id="password">
+            </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+    </div>
+</div>
 @endsection
 @section('addJs')
     {{-- Masukkan dibawah ini jika ingin menambahkan JS --}}
