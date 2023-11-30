05d294ab08e1090b3491e613cda6af2ae7f735a8
Thahirudin <tohiruzain098@gmail.com>
2023-11-30 15:46:43 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -33,9 +33,7 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::get('/admin/tambah-event', [EventController::class, 'adminCreate'])->name('admin-tambah-event');
     Route::post('/admin/tambah-event', [EventController::class, 'store'])->name('admin-store-event');
     // order
-    Route::get('/admin/list-order', function () {
-        return view('admin/order');
-    })->name('admin-list-order');
+    Route::get('/admin/list-order', [OrderController::class, 'index'])->name('admin-list-order');
     Route::get('/admin/tambah-order', [OrderController::class, 'adminCreate'])->name('admin-tambah-order');
     Route::post('/admin/tambah-order', [OrderController::class, 'store'])->name('admin-store-order');
     Route::get('/admin/edit-order', [OrderController::class, 'adminEdit'])->name('admin-edit-order');
@@ -49,9 +47,7 @@ Route::middleware(['auth', 'admin'])->group(function () {
     Route::post('/admin/tambah-organizer', [OrganizerController::class, 'store'])->name('admin-store-organizer');
     Route::get('/admin/edit-organizer', [OrganizerController::class, 'adminEdit'])->name('admin-edit-organizer');
     // member
-    Route::get('/admin/list-member', function () {
-        return view('admin/member');
-    })->name('admin-list-member');
+    Route::get('/admin/list-member', [MemberController::class, 'index'])->name('admin-list-member');
     Route::get('/admin/tambah-member', [MemberController::class, 'adminCreate'])->name('admin-tambah-member');
     Route::post('/admin/tambah-member', [MemberController::class, 'store'])->name('admin-store-member');
     Route::get('/admin/edit-member', [MemberController::class, 'adminEdit'])->name('admin-edit-member');
