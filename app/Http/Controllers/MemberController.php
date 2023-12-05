c0c1568db606cc0f2dbed530971de0202a9b98dc
Thahirudin <tohiruzain098@gmail.com>
2023-12-05 10:43:13 +0700

    perbaikan edit member

--- app/Http/Controllers/MemberController.php
+++ app/Http/Controllers/MemberController.php
@@ -66,41 +66,63 @@ class MemberController extends Controller
         // Create a new Organizer instance and save it to the database
 
     }
-    function update(Request $request, $id){
-    // Validate the incoming request data
-    $request->validate([
-        'nama' => 'required|string|max:255',
-        'profil' => 'required',
-        'tanggal_lahir' => 'required|date',
-        'email' => 'required|email|unique:tbl_members',
-        'password' => 'required',
-    ]);
-    DB::beginTransaction(); // Mulai transaksi database
-    $hashedPassword = Hash::make($request->password);
-    try {
-        $image = $request->file('profil');
-        $imageName = now()->format('YmdHis') . '-' . $request->nama . '.' . $image->getClientOriginalExtension();
-        $image->move(public_path('uploads/members'), $imageName);
-        $member = Member::find($id);
-        $member->update([
-            'nama' => $request->nama,
-            'profil' => $imageName,
-            'tanggal_lahir' => $request->tanggal_lahir,
-            'email' => $request->email,
-            'password' => $hashedPassword,
-        ]);
+    function update(Request $request, $id)
+    {
+        try {
+            DB::beginTransaction();
 
-        DB::commit(); // Commit transaksi database
+            // Validate the incoming request data
+            $request->validate([
+                'nama' => 'required|string|max:255',
+                'tanggal_lahir' => 'required|date',
+                'email' => 'required|email|unique:tbl_members,email,' . $id,
+            ]);
 
-        // Redirect back or to a success page
-        return redirect(route('admin-list-member'))->with('sukses', 'Member Berhasil Diedit');
+            $member = Member::find($id);
 
-    } catch (QueryException $e) {
-        DB::rollBack();
-        return redirect()->back()->with('error', $e->getMessage());
+            if (!$member) {
+                throw new \Exception('Member tidak ditemukan');
+            }
+
+            // Check if a new profile image is uploaded
+            if ($request->hasFile('profil')) {
+                // Delete the old profile image if it exists
+                if ($member->profil) {
+                    $oldImagePath = public_path('uploads/members/' . $member->profil);
+                    if (file_exists($oldImagePath)) {
+                        unlink($oldImagePath);
+                    }
+                }
+
+                // Upload the new profile image
+                $image = $request->file('profil');
+                $imageName = now()->format('YmdHis') . '-' . $request->nama . '.' . $image->getClientOriginalExtension();
+                $image->move(public_path('uploads/members'), $imageName);
+
+                // Update member data including the new profile image
+                $member->update([
+                    'nama' => $request->nama,
+                    'profil' => $imageName,
+                    'tanggal_lahir' => $request->tanggal_lahir,
+                    'email' => $request->email,
+                    'password' => $request->password ? Hash::make($request->password) : $member->password,
+                ]);
+            } else {
+                // Use the existing profile image
+                $member->update([
+                    'nama' => $request->nama,
+                    'tanggal_lahir' => $request->tanggal_lahir,
+                    'email' => $request->email,
+                    'password' => $request->password ? Hash::make($request->password) : $member->password,
+                ]);
+            }
+
+            DB::commit(); // Commit the transaction
+
+            return redirect('/admin/list-member')->with('sukses', 'Member Berhasil DiEdit');
+        } catch (\Exception $e) {
+            DB::rollBack(); // Rollback the transaction in case of an exception
+            return redirect()->back()->with('error', 'Gagal mengedit member. ' . $e->getMessage());
+        }
     }
-
-    // Create a new Organizer instance and save it to the database
-
-}
 }
